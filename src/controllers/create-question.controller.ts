a61d9a973d5ccf1515de33adce102cd60487cbca
Felipe Sakai Ipolito <167255155+FelipeSakai@users.noreply.github.com>
2026-06-23 07:54:36 -0400

    add question slug generation

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,24 +1,53 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CurrentUser } from 'src/auth/current-user-decoretor';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import type { UserPayload } from 'src/auth/jwt.strategy';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
 import { PrismaService } from 'src/prisma/prisma.service';
-import z from 'zod';
+import { z } from 'zod';
 
 const createQuestionBodySchema = z.object({
     title: z.string().max(100),
     content: z.string().max(500),
 });
 
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+
 type CreateQuestionBody = z.infer<typeof createQuestionBodySchema>;
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-    constructor(
-        private readonly prisma: PrismaService
-    ) { }
+    constructor(private prisma: PrismaService) { }
 
     @Post()
-    async handle() {
-        return { message: 'Question created successfully' };
+    async handle(
+        @Body(bodyValidationPipe) body: CreateQuestionBody,
+        @CurrentUser() user: UserPayload
+    ) {
+        const { title, content } = body;
+        const userId = user.sub;
+
+        const slug = this.convertToSlug(title);
+
+        await this.prisma.question.create({
+            data: {
+                authorId: userId,
+                title,
+                content,
+                slug,
+            }
+        });
+    }
+
+    private convertToSlug(value: string) {
+        return value
+            .normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, '')
+            .toLowerCase()
+            .trim()
+            .replace(/[^a-z0-9\s-]/g, '')
+            .replace(/\s+/g, '-')
+            .replace(/-+/g, '-');
     }
 }
